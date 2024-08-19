402700b5f4074721db68f3d21b81c4040015f6a7
Syed Mehdi <syed@bcw.group>
2024-08-19 10:15:49 +0500

    trigger call

--- representative-usage-hardhat/scripts/TestBench.ts
+++ representative-usage-hardhat/scripts/TestBench.ts
@@ -4,23 +4,23 @@ import ScriptConfig from "./scriptConfig";
 import hre from "hardhat";
 import { deployAll } from "./deploy/deployAll";
 import { TEST_ACCOUNTS } from "../testaccounts";
+import { RPCTriggerCall } from "./types/rpc.triggercall";
 
-const performPassportGemGameCall = async (owner_a: Wallet, nonce: number) => {
+const performPassportGemGameCall = async (owner_a: Wallet): Promise<RPCTriggerCall> => {
     const salt = encodeImageHash(1, [{ weight: 1, owner: owner_a }]);
-    console.log('Salt:', salt);
 
     const { factory, mainModule, multiCall } = ScriptConfig.passportWallet;
 
     // Calculate CFA (Contract Factory Address) based on the salt
     const cfa = addressOf(factory.target.toString(), mainModule.target.toString(), salt);
-    console.log('CFA:', cfa);
-    console.log('Signer:', owner_a.address);
+
+    const nonce = await hre.ethers.provider.getTransactionCount(owner_a.address);
 
     // Prepare the transaction data for calling `earnGem` on the gemGame contract
     const transaction = {
         delegateCall: false,
         revertOnError: true,
-        gasLimit: "0", 
+        gasLimit: "0",
         target: ScriptConfig.gemGame.target.toString(),
         value: "0",
         data: ScriptConfig.gemGame.interface.encodeFunctionData('earnGem')  // Encode the call to earnGem
@@ -39,30 +39,43 @@ const performPassportGemGameCall = async (owner_a: Wallet, nonce: number) => {
     const executorRole = await multiCall.EXECUTOR_ROLE();
 
     const roleTx = await multiCall.connect(admin).grantRole(executorRole, owner_a.address)
-    await roleTx.wait()
+    await roleTx.wait();
 
+    const trigger = async () => {
+        const tx = await multiCall.connect(owner_a).deployAndExecute(cfa, mainModule.target, salt, factory.target, transactions, nonce, sig);
+        const receipt = await tx.wait();
+        console.log('Transaction Hash:', receipt?.hash);
+        console.log('Block Number:', receipt?.blockNumber);
+    }
 
-    // const grantRoleTx = await ScriptConfig.accessControlledDeployer.connect(admin).grantDeployerRole([owner_a.address]);
-    // await grantRoleTx.wait();
-    
-    const tx = await multiCall.connect(owner_a).deployAndExecute(cfa, mainModule.target, salt, factory.target, transactions, nonce, sig);
-    const receipt = await tx.wait();
+    return {
+        trigger
+    }
 
-
-
-    console.log('Transaction Hash:', receipt?.hash);
-    console.log('Block Number:', receipt?.blockNumber);
 };
 
 const main = async () => {
 
     const treasurySigner = await hre.ethers.provider.getSigner(1);
     const runName = "run1";
+    console.log("Deploying all contracts...");
     const resp = await deployAll(treasurySigner, runName, false);
+    console.log("Contracts deployed successfully!");
 
+    console.log("Booting...")
+    const bootTimeStart = Date.now();
     const owner_a = new Wallet(TEST_ACCOUNTS[112], hre.ethers.provider);
     console.log('Owner A:', owner_a.address);
-    await performPassportGemGameCall(owner_a);
+    const { trigger: passportGem112 } = await performPassportGemGameCall(owner_a);
+    const bootTimeEnd = Date.now();
+
+    console.log('Boot Time:', bootTimeEnd - bootTimeStart);
+
+    const triggerTime = Date.now();
+    console.log('Triggering Passport Gem Game Call for Owner A');
+    await passportGem112();
+    console.log('Trigger Time:', Date.now() - triggerTime);
+
 }
 
 
